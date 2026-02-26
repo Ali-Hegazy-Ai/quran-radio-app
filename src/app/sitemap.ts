c78f399269f5fbb009791abe4ffd772a7b4ca6be
ali-hegazy-ai <ali_hegazy_@outlook.com>
2026-02-26 03:22:42 +0200

    chore: Add .vercel to gitignore.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const SITE_URL = 'https://quran-radio.vercel.app'
+    return [
+        {
+            url: SITE_URL,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1,
+        }
+    ]
+}
